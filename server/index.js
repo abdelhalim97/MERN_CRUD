be8edcf5b9cc8f5e46bef54b3f804dab0782d934
abdelhalim97 <abdelhalimbenoun@gmail.com>
2022-03-25 02:06:30 +0100

    env

--- server/index.js
+++ server/index.js
@@ -3,13 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import routers from './routes/projects.js'
+import dotenv from 'dotenv'
+
 const app=express()
+dotenv.config()
 app.use(bodyParser.json({extended:true,limit:'50mb'})) //calling middlewares |extended => bodparser will include any tpe of data not just strings/we can add ,limit:"30mb"
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 app.use('/',routers)
-const CONNECTION__URL=process.env.CONNECTION__URL||'mongodb+srv://abdelhalim:douaa123@cluster0.sor4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.PORT||5000;
+const CONNECTION__URL=process.env.CONNECTION__URL
+const PORT = process.env.PORT;
 mongoose.connect(CONNECTION__URL)
     .then(()=>app.listen(PORT,()=>console.log('server running')))
     .catch((err)=>console.log(err))
